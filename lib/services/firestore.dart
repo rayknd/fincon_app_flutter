93506c5959e5a75b8f125e975049ca846aee9869
olavobelfort <olavobelfort@outlook.com>
2023-10-25 01:12:35 -0300

    feat: Expense communication with firebase

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -1,9 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fincon_app/models/fixed_expenses.dart';
 
 class FirestoreService {
   //get collection of categories
   final CollectionReference _categories =
       FirebaseFirestore.instance.collection("categories");
+  final CollectionReference _expenses =
+      FirebaseFirestore.instance.collection("expenses");
 
   //Add new category
   Future<void> addCategory(String categoryName) {
@@ -27,5 +30,28 @@ class FirestoreService {
   //Delete category
   Future<void> deleteCategory(String docID) {
     return _categories.doc(docID).delete();
+  }
+
+  Future<void> addExpense(Expense expense) {
+    return _expenses.add(expense.toJson());
+  }
+
+
+  Stream<QuerySnapshot> getExpenseStream() {
+    final expensesStream =
+        _expenses.where('expense_dtmExclusion', isEqualTo: "").snapshots();
+    return expensesStream;
+  }
+
+  //Update category
+  Future<void> updateExpense(String docID, Expense newExpense) {
+    return _expenses.doc(docID).update(newExpense.toJson());
+  }
+
+  //Delete category
+  Future<void> deleteExpense(String docID) {
+    return _expenses.doc(docID).update({
+      "expense_dtmExclusion":  DateTime.now(),
+    });
   }
 }
