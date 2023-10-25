1353fab566758f9b276b0c04a9e599ea8a092e63
olavobelfort <olavobelfort@outlook.com>
2023-10-25 12:54:48 -0300

    feat: attribute expense_dtmPayment and factory for Expense

--- lib/models/fixed_expenses.dart
+++ lib/models/fixed_expenses.dart
@@ -1,13 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Expense {
   final String name;
   final String category;
   final String value;
   final DateTime date;
   final bool recurrent;
-  final String dtmExclusion;
+  final DateTime? dtmExclusion;
   final String userId;
+  DateTime? dtmPayment;
 
-  Expense(this.name, this.category, this.value, this.date, this.recurrent, this.dtmExclusion, this.userId);
+  Expense(this.name, this.category, this.value, this.date, this.recurrent,
+      this.dtmExclusion, this.userId, this.dtmPayment);
 
   Map<String, dynamic> toJson() => {
         "expense_name": name,
@@ -17,5 +21,28 @@ class Expense {
         "expense_recurrent": recurrent,
         "expense_dtmExclusion": dtmExclusion,
         "expense_userId": userId,
+        "expense_dtmPayment": dtmPayment,
       };
+
+  factory Expense.fromJson(Map<String, dynamic> json) {
+    DateTime? dtmExclusion;
+    if(json["expense_dtmExclusion"] is Timestamp){
+      dtmExclusion = (json["expense_dtmExclusion"] as Timestamp).toDate();
+    }
+
+    DateTime? dtmPayment;
+    if(json["expense_dtmPayment"] is Timestamp){
+      dtmPayment = (json["expense_dtmPayment"] as Timestamp).toDate();
+    }
+
+    return Expense(
+        json["expense_name"],
+        json["expense_category"],
+        json["expense_value"],
+        (json["expense_date"] as Timestamp).toDate(),
+        json["expense_recurrent"],
+        dtmExclusion,
+        json["expense_userId"],
+        dtmPayment);
+  }
 }
